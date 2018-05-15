b910fbcbe89036c8c6211463a03e36cdfd97dcb9
kyana1nen <34677447+kyana1nen@users.noreply.github.com>
2018-05-15 17:47:09 +0900

    Add files via upload

---
+++ click.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class click : MonoBehaviour {
+	public GameObject enemy;
+	void Start () {
+		for (int i = 0; i > 10; i++) {
+			Instantiate (enemy);
+		}
+	}
+	void Update () {
+		
+	}
+    void OnMouseDown () {
+		Destroy (gameObject);
+    }
+}
---
+++ jump.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class jump : MonoBehaviour {
+	Rigidbody rb;
+	Transform y;
+	float t=20;
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	void Update () {
+		t = t - Time.deltaTime;
+		Debug.Log (t);
+	}
+	void OnCollisionEnter (Collision arg) {
+		if (arg.gameObject.tag == "floor") {
+			Vector3 y = new Vector3 (0, Random.Range(300,700), 0);
+			rb.AddForce (y);
+		}
+	}
+}
